b9c7a0f62942b9fe1ffb3bd71cf9d8defbb403c9
Eric McKay <ericmckay4@gmail.com>
2023-10-06 09:41:37 -0400

    get rid of open-graph-scraper

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
-import ogScrape from "open-graph-scraper";
 import * as http from "follow-redirects";
-import { OgObject } from "open-graph-scraper/dist/lib/types";
 import { DebugLogger } from "../util/debug-logger";
+import {JSDOM} from 'jsdom'
 
 export const urlRoute = async (req: Request, res: Response) => {
   const debug = new DebugLogger();
@@ -10,7 +9,7 @@ export const urlRoute = async (req: Request, res: Response) => {
   debug.log("urlRoute - begin", { url });
   if (
     !/[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi.test(
-      url,
+      url
     )
   ) {
     return res
@@ -18,73 +17,67 @@ export const urlRoute = async (req: Request, res: Response) => {
       .send(JSON.stringify({ status: 400, error: "No URL detected" }));
   }
   debug.log("  getFinalBaseUrl - begin", { url });
-  const finalUrl = await getFinalBaseUrl(url);
+  const { urlOrigin, html } = await getRedirectedPage(url);
   debug.log("  getFinalBaseUrl - success", { url });
-  // TODO: add timeout
-  // TODO: cache this so it doesn't get fetched twice
-  debug.log("  ogScrape - begin", { url });
-  const { error, result } = await ogScrape({ url: finalUrl });
-  debug.log("  ogScrape - success", { url });
 
-  if (error) {
-    return res.status(400).send(
-      JSON.stringify({
-        status: 400,
-        error: "Error retrieving og from source url",
-      }),
-    );
-  }
+  debug.log("  extractMetaTags - begin", { url });
+  const metaTags = extractMetaTags(html);
+  debug.log("  extractMetaTags - success", { url });
 
-  const metasToUpdate = {
-    twitterImage: result.twitterImage,
-    ogImage: result.ogImage,
-  };
-  const urlRoot = new URL(finalUrl).origin
+  const urlRoot = new URL(urlOrigin).origin
     .replace("www.", "")
     .replace(/^https?:\/\//, "");
 
-  // TODO: encode ? in image urls to prevent double ? in routes
-  metasToUpdate.twitterImage = metasToUpdate.twitterImage?.map((img) => ({
-    ...img,
-    url: appendParamsToUrl(`${process.env.URL_ORIGIN}/img/${img.url}`, {
-      title: result.twitterTitle || result.ogTitle,
-      description: result.twitterDescription || result.ogDescription,
-      urlRoot,
-    }),
-  }));
-  metasToUpdate.ogImage = metasToUpdate.ogImage?.map((img) => ({
-    ...img,
-    url: appendParamsToUrl(`${process.env.URL_ORIGIN}/img/${img.url}`, {
-      title: result.ogTitle,
-      description: result.ogDescription,
-      urlRoot,
-    }),
-  }));
+  const twitterDescription = metaTags.findLast(
+    (t) =>
+      t.name === "twitter:description" || t.property === "twitter:description"
+  )?.content;
+  const ogDescription = metaTags.findLast(
+    (t) => t.name === "og:description" || t.property === "og:description"
+  )?.content;
+  const twitterTitle = metaTags.findLast(
+    (t) => t.name === "twitter:title" || t.property === "twitter:title"
+  )?.content;
+  const ogTitle = metaTags.findLast(
+    (t) => t.name === "og:title" || t.property === "og:title"
+  )?.content;
+
+  const proxiedMetaTags = metaTags.map((t) => {
+    if (!t.content) return t;
+    if (t.name === "twitter:image" || t.property === "twitter:image") {
+      return {
+        ...t,
+        content: imgRoute(t.content, {
+          title: twitterTitle ?? ogTitle,
+          description: twitterDescription ?? ogDescription,
+          urlRoot,
+        }),
+      };
+    }
+    if (t.name === "og:image" || t.property === "og:image") {
+      return {
+        ...t,
+        content: imgRoute(t.content, {
+          title: ogTitle,
+          description: ogDescription,
+          urlRoot,
+        }),
+      };
+    }
+    return t;
+  });
 
   // regenerate original meta tags
-  const metaTags = Object.entries(result)
-    .map(([key, value]) => {
-      if (key in metasToUpdate) {
-        return generateMetaTags(
-          key,
-          metasToUpdate[key as keyof typeof metasToUpdate],
-        );
-      }
-      return generateMetaTags(key, value);
-    })
-    .filter((tag) => tag)
-    .join("\n");
-
   const htmlResponse = `<!DOCTYPE html>
 <html lang="en">
 <head>
-	${metaTags}
+	${proxiedMetaTags.map((t) => metaTagToHtml(t)).join("\n  ")}
 	<script type="text/javascript">
-		// window.location.href = "${finalUrl}";
+		// window.location.href = "${url}";
 	</script>
 </head>
 <body>
-	If you are not redirected automatically, follow this <a href="${finalUrl}">link</a>.
+	If you are not redirected automatically, follow this <a href="${url}">link</a>.
 </body>
 </html>`;
 
@@ -92,46 +85,27 @@ export const urlRoute = async (req: Request, res: Response) => {
   // Send HTML response
   return res.send(htmlResponse);
 };
-const toColonCase = (str: string) => {
-  return str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1:$2").toLowerCase();
-};
 
-const generateMetaTags = (key: string, value: OgObject[keyof OgObject]) => {
-  const formattedKey = toColonCase(key);
-  if (Array.isArray(value)) {
-    return value
-      .map((item) => {
-        if (typeof item === "object") {
-          return `<meta property="${formattedKey}" name="${formattedKey}" content="${item.url}">`;
-        }
-        return `<meta property="${formattedKey}" name="${formattedKey}" content="${item}">`;
-      })
-      .join("\n");
-  } else if (typeof value === "object") {
-    if ("url" in value || "content" in value) {
-      return `<meta property="${formattedKey}" name="${formattedKey}" content="${
-        value.url || value.content
-      }">`;
-    }
-    return "";
-  } else if (value) {
-    return `<meta property="${formattedKey}" name="${formattedKey}" content="${value}">`;
-  }
-  return "";
-};
-
-const getFinalBaseUrl = async (url: string): Promise<string> => {
+const getRedirectedPage = async (
+  url: string
+): Promise<{ urlOrigin: string; html: string }> => {
   return new Promise((resolve, reject) => {
     const isHttps = url.startsWith("https");
     const client = isHttps ? http.https : http.http;
-
     client
       .get(url, (response) => {
-        if (response.responseUrl) {
-          resolve(response.responseUrl);
-        } else {
-          reject(new Error("Failed to obtain the final URL"));
-        }
+        let html = "";
+        response.on("data", (chunk) => {
+          html += chunk;
+        });
+        response.on("end", () => {
+          if (response.responseUrl) {
+            const urlOrigin = new URL(response.responseUrl).origin;
+            resolve({ urlOrigin, html });
+          } else {
+            reject(new Error("Failed to obtain the final URL"));
+          }
+        });
       })
       .on("error", (error) => {
         reject(error);
@@ -141,7 +115,7 @@ const getFinalBaseUrl = async (url: string): Promise<string> => {
 
 const appendParamsToUrl = (
   url: string,
-  params: Record<string, string | number | boolean | undefined>,
+  params: Record<string, string | number | boolean | undefined>
 ): string => {
   const urlObject = new URL(url);
   const sanitizedParams: Record<string, string> = {};
@@ -151,4 +125,46 @@ const appendParamsToUrl = (
   const searchParams = new URLSearchParams(sanitizedParams);
   urlObject.search = searchParams.toString();
   return urlObject.toString();
+};
+
+const imgRoute = (
+  src: string,
+  params: { [key: string]: string | undefined }
+) => {
+  // TODO: encode ? in image urls to prevent double ? in routes
+  return appendParamsToUrl(`${process.env.URL_ORIGIN}/img/${src}`, params);
+};
+
+type MetaTagData = {
+  name?: string;
+  property?: string;
+  content?: string;
+  [key: string]: string | undefined;
+};
+
+const extractMetaTags = (htmlString: string): MetaTagData[] => {
+	const dom = new JSDOM(htmlString);
+	const document = dom.window.document;
+	const metaTags = document.querySelectorAll('meta');
+
+  return Array.from(metaTags)
+    .map((tag) => {
+      const data: MetaTagData = {};
+
+      Array.from(tag.attributes).forEach((attr) => {
+        data[attr.name] = attr.value;
+      });
+
+      return data;
+    })
+    .filter((data) => Object.keys(data).length > 0);
+};
+
+const metaTagToHtml = (data: MetaTagData) => {
+  const attributes = Object.entries(data)
+    .filter(([, value]) => value !== undefined)
+    .map(([key, value]) => `${key}="${value}"`)
+    .join(" ");
+
+  return `<meta ${attributes} />`;
 };
