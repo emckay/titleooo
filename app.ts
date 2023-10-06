c065ab394d8ca1798471720d0f65da9fddd38ca3
Eric McKay <ericmckay4@gmail.com>
2023-10-05 21:15:08 -0400

    add favicon

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { homeRoute } from "./routes/home";
 import { imageRoute } from "./routes/image";
 import { urlRoute } from "./routes/url";
+import path from "path";
 import dotenv from "dotenv-flow";
 dotenv.config();
 
@@ -9,6 +10,9 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", homeRoute);
+app.get("/favicon.ico", (req, res) => {
+  return res.sendFile(path.join(__dirname, "assets/favicon.ico"));
+});
 app.get("/img/*", imageRoute);
 app.get("/*", urlRoute);
 
